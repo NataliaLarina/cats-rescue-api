827c17ca561345fccaae54c83137caac5e7aa581
Michael Sturrus <msturrus@gmail.com>
2016-11-14 15:07:02 -0600

    hurumph

--- client/public/javascripts/app.js
+++ client/public/javascripts/app.js
@@ -1,42 +1,86 @@
-console.log("hobbits and whatever")
+console.log('hello, hobbit');
 
-var CatsApp = angular.module('CatsApp', ['ngRoute'])
+var CatsApp = angular.module('CatsApp', []);
 
-// $scope = this
 CatsApp.controller('CatsCtrl', function($scope, $http) {
-  $scope.cats = []
-  $scope.urlHost = 'http://localhost:3000/'
-  $scope.addCat = function(name, note) {
-    var cat = {name: name, note: note}
-    $scope.cats.push(cat)
-    console.log($scope.cats)
-  }
+  $scope.cats = [];
+  $scope.urlHost = 'http://localhost:3000';
+  $scope.message = '';
 
   $scope.addCat = function(name, note) {
-    $http({
+    // var cat = {name: name, note: note};
+    // $scope.cats.push(cat);
+    // console.log($scope.cats);
+    $http( {
+      url: $scope.urlHost + '/cats/cat',
       method: 'post',
-      url: $scope.urlHost + 'cats/cat',
-      name: name,
-      note: note
-      // params: { name: name, note: note }
-    }).success(function(data){
-      console.log(data)
-      $scope.populateList()
+      data: {name: name, note: note}
+    }).success(function(data) {
+      console.log(data);
+      $scope.populateList();
     })
   }
 
   $scope.populateList = function() {
-    $http.get($scope.urlHost + 'cats/cats').success(function(data){
-        console.log(data)
-        $scope.cats = data
-    })
+    $http.get($scope.urlHost + '/cats/cats').success(function(data) {
+      console.log(data);
+      $scope.cats = data;
+    });
   }
 
   $scope.adoptCat = function(id) {
-    $http.delete($scope.urlHost + 'cats/cat/' + id).success(function(data){
-        console.log(data)
+    console.log(id);
+    $http({
+      url: $scope.urlHost + '/api/cats/' + id,
+      method: 'delete'
+    }).success(function(data) {
+      console.log('Your cat is adopted');
+      $scope.message = data.message;
     })
   }
 
+});
 
-})
+// console.log("hobbits and whatever")
+//
+// var CatsApp = angular.module('CatsApp', ['ngRoute'])
+//
+// // $scope = this
+// CatsApp.controller('CatsCtrl', function($scope, $http) {
+//   $scope.cats = []
+//   $scope.urlHost = 'http://localhost:3000/'
+//   // $scope.addCat = function(name, note) {
+//   //   var cat = {name: name, note: note}
+//   //   $scope.cats.push(cat)
+//   //   console.log($scope.cats)
+//   // }
+//
+//   $scope.addCat = function(name, note) {
+//     var cat = {name: name, note: note}
+//     $scope.cats.push(cat)
+//     $http({
+//       method: 'post',
+//       url: $scope.urlHost + 'cats/cat',
+//       data: cat
+//       // params: { name: name, note: note }
+//     }).success(function(data){
+//       console.log(data)
+//       $scope.populateList()
+//     })
+//   }
+//
+//   $scope.populateList = function() {
+//     $http.get($scope.urlHost + 'cats/cats').success(function(data){
+//         console.log(data)
+//         $scope.cats = data
+//     })
+//   }
+//
+//   $scope.adoptCat = function(id) {
+//     $http.delete($scope.urlHost + 'cats/cat/' + id).success(function(data){
+//         console.log(data)
+//     })
+//   }
+//
+//
+// })
