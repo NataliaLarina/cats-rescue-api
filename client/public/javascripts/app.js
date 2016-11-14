7f42a338ae9a055b5811fd35f758de34d78e2c9b
Michael Sturrus <msturrus@gmail.com>
2016-11-14 12:28:01 -0600

    working thing for the thing

--- client/public/javascripts/app.js
+++ client/public/javascripts/app.js
@@ -1,1 +1,13 @@
 console.log("hobbits and whatever")
+
+var CatsApp = angular.module('CatsApp', ['ngRoute'])
+
+// $scope = this
+CatsApp.controller('CatsCtrl', function($scope) {
+  $scope.cats = []
+  $scope.addCat = function(name, note) {
+    var cat = {name: name, note: note}
+    $scope.cats.push(cat)
+    console.log($scope.cats)
+  }
+})
